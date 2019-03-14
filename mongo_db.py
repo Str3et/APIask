375b62d1ece43b18a64bd56efe2d90753c387731
Str3et <paanika@list.ru>
2019-03-14 09:18:44 +0300

    Добавлен поиск по аккаунту.

--- mongo_db.py
+++ mongo_db.py
@@ -3,4 +3,5 @@ from pymongo import MongoClient
 
 client = MongoClient()
 database = client.database
-email_db = database.account
+email_db = database.email
+account_db = database.account
--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import re
 import os
+import requests
+from bs4 import BeautifulSoup
+
 from selenium import webdriver
 
-from config import BASE_URL
-from mongo_db import email_db
+from config import BASE_URL_EMAIL, BASE_URL_ACCOUNT, TITLE_ACCOUNT
+from mongo_db import email_db, account_db
 import time
 
 webdriver_settings = webdriver.ChromeOptions()
@@ -20,7 +23,7 @@ def test_email_reg(email):
     if mail:
         # вэбдрайвер Chrome для селениума
         driver_browser = webdriver.Chrome(os.getcwd() + '/chromedriver', chrome_options=webdriver_settings)
-        driver_browser.get(BASE_URL)
+        driver_browser.get(BASE_URL_EMAIL)
         form_email = driver_browser.find_element_by_class_name('inputForm')  # поиск по html
         form_email.send_keys(email)  # ввод email`a
         form_email.submit()  # отправка формы
@@ -41,5 +44,21 @@ def test_email_reg(email):
 
 
 # Добавление нового документа в БД
-def add_result(email, status):
+def add_email_result(email, status):
     email_db.insert_one({'user_registration_data': email, 'status': status})
+
+
+def add_account_result(account, status):
+    email_db.insert_one({'user_registration_data': account, 'status': status})
+
+
+def test_account_reg(account_name):
+    # post_result = requests.post(BASE_URL_ACCOUNT, data=account_name)
+    test = requests.get(BASE_URL_ACCOUNT + account_name)
+    dom = BeautifulSoup(test.text)  # получаем html страницы
+    title = str(dom.select('title')[0])  # забираем строку tittle`a
+    if title == TITLE_ACCOUNT:
+        return False
+    else:
+        return True
+
